3197af63430a588fdcfea9518ee90e4823ed07d1
张庭勇 <wanmeizty@dingtalk.com>
2021-12-13 11:45:23 +0800

    no message

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -33,39 +33,7 @@
 </script>
 
 <style lang="scss" scoped>
-	.Grid {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-		align-content: space-between;
-		background: #f7f7f7;
-		padding:10rpx 0;
-		.Grid-Item {
-			width: 50%;
-			height: 213rpx;
-			text-align: center;
-			border:1rpx solid #ccc;
-			box-sizing:border-box;
-			.GSimg {
-				width: 96rpx;
-				height: 96rpx;
-				margin-top: 42rpx;
-				margin-left: 50rpx;
-				.Image {
-					width: 96rpx;
-					height: 96rpx;
-				}
-			}
-			.GStitle {
-				width: 100%;
-				height: 34rpx;
-				line-height: 34rpx;
-				color: #06121e;
-				font-size: 24rpx;
-				margin-top: 20rpx;
-			}
-		}
-	}
+	
 </style>
 
 
