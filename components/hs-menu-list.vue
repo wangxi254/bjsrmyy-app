f2b16c511fa8fea23d551d7f2aab7141db5a6c4f
张庭勇 <wanmeizty@dingtalk.com>
2021-12-13 11:39:11 +0800

    列表

---
+++ components/hs-menu-list.vue
@@ -0,0 +1,74 @@
+<template>
+	<view>
+		<view class="Grid">
+			<view class="Grid-Item" v-for="item in List" :key="item.id" @click="onClick(item)">
+				<view class="GSimg"><image class="Image" :src="item.img"></image></view>
+			    <view>
+					<view class="GStitle">{{ item.title }}</view>
+					<view v-if="item.detail" class="GSDetail">{{ item.detail }}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'UniSteps',
+		components: {
+			/* uniIcons */
+		},
+		props: {
+			List:{
+				type:Array,
+				default:[]
+			},
+		},
+		data() {
+			return {}
+		},
+		methods:{
+			
+			onClick(item){
+				this.$emit('menuClick', item);
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.Grid {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		align-content: space-between;
+		background: #f7f7f7;
+		padding:20px 0;
+		.Grid-Item {
+			width: 50%;
+			height: 70px;
+			// text-align: center;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			border:1rpx solid #ccc;
+			box-sizing:border-box;
+			.GSimg {
+				width: 96rpx;
+				height: 96rpx;
+				.Image {
+					width: 96rpx;
+					height: 96rpx;
+				}
+			}
+			.GStitle {
+				width: 100%;
+				height: 34rpx;
+				line-height: 34rpx;
+				color: #06121e;
+				font-size: 24rpx;
+			}
+		}
+	}
+</style>
+
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,71 @@
 <template>
 	<view>
-		
+		<hsMenuList :List="dataList" @menuClick="menuClick" />
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
+	import hsMenuList from "../../components/hs-menu-list.vue";
+	export default{
+		data(){
 			return {
-
+				dataList:[
+						{id:1,img:'/static/tabbar/home.png',title:'医院介绍',detail:'appointment'},
+						{id:2,img:'/static/tabbar/home.png',title:'出诊安排',detail:'appointment'},
+						{id:3,img:'/static/tabbar/home.png',title:'就诊指南',detail:'appointment'},
+						{id:4,img:'/static/tabbar/home.png',title:'科室介绍',detail:'appointment'},
+						{id:5,img:'/static/tabbar/home.png',title:'专家介绍',detail:'appointment'},
+						{id:6,img:'/static/tabbar/home.png',title:'楼层导航',detail:'appointment'},
+						{id:7,img:'/static/tabbar/home.png',title:'健康百科',detail:''},
+						{id:8,img:'/static/tabbar/home.png',title:'健康咨询',detail:'appointment'}
+				   ]
 			}
 		},
-		onLoad() {
-
+		components:{
+			hsMenuList
 		},
-		methods: {
-
+		methods:{
+			menuClick(item){
+				console.log("item",JSON.stringify(item));
+			}
 		}
 	}
 </script>
 
-<style>
-	
+<style lang="scss" scoped>
+	.Grid {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		align-content: space-between;
+		background: #f7f7f7;
+		padding:10rpx 0;
+		.Grid-Item {
+			width: 50%;
+			height: 213rpx;
+			text-align: center;
+			border:1rpx solid #ccc;
+			box-sizing:border-box;
+			.GSimg {
+				width: 96rpx;
+				height: 96rpx;
+				margin-top: 42rpx;
+				margin-left: 50rpx;
+				.Image {
+					width: 96rpx;
+					height: 96rpx;
+				}
+			}
+			.GStitle {
+				width: 100%;
+				height: 34rpx;
+				line-height: 34rpx;
+				color: #06121e;
+				font-size: 24rpx;
+				margin-top: 20rpx;
+			}
+		}
+	}
 </style>
+
+
