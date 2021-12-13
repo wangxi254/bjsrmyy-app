056c046ac836f57d863a6d03c6a13b858a641c7d
张庭勇 <wanmeizty@dingtalk.com>
2021-12-13 11:55:11 +0800

    no message

---
+++ pages/yx/mine.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.flex-row{
+		display: flex;
+		flex-direction: row;
+	}
+</style>
