d36c92697b01116e9c8bab975dc1efa22ae7e39c
张庭勇 <wanmeizty@dingtalk.com>
2021-12-22 23:08:43 +0800

    no message

--- hospitalIntro/expertorList.vue
+++ hospitalIntro/expertorList.vue
@@ -20,6 +20,7 @@
 					<view class="flex-row">
 						<view>{{item.docName}}</view>
 						<view style="margin-left: 10px;">{{item.docTitle}}</view>
+						<view class="flex1"  @click.stop="usrfav(item)">关注</view>
 					</view>
 					<view class="expertor-pro">
 						<text class="">擅长：</text>{{item.special}}
@@ -73,6 +74,27 @@
 						endDate:date,
 						depCode:depCode
 					}
+				}).then(res=>{
+					if(res.data.code == 200){
+						this.expertorList = res.data.data;
+					}
+				})
+			},
+			usrfav(item){
+				if(!uni.getStorageSync("userId")){
+					uni.navigateTo({
+						url:"../pages/auth/auth"
+					})
+				}
+				this.$request({
+					path:'/userfav/doctor',
+					query:{
+						userId:uni.getStorageSync("userId"),
+						docCode:item.docCode,
+						docTitle:item.docTitle,
+						special:item.special,
+					},
+					method:'POST',
 				}).then(res=>{
 					if(res.data.code == 200){
 						this.expertorList = res.data.data;
