67022bef08331966a7cbf5be0b560eef067e1de9
前端-谭浩 <542443538@qq.com>
2021-12-23 21:45:15 +0800

    11

--- guidance/guidanceList.vue
+++ guidance/guidanceList.vue
@@ -22,6 +22,7 @@
 			<view class="bzlist">
 				<view class="title">选择科室</view>
 				<view class="list">
+					<NoData v-if="current.length === 0" />
 					<picker @change="bindPickerChange" :value="type" :range="array">
 						<view @click="chooseDep(i)" v-for="i in current" :key="i.depCode" class="item">{{i.depName}}</view>
 					</picker>
@@ -32,7 +33,9 @@
 </template>
 
 <script>
+	import NoData from '@/components/nodata/index.vue' 
 	export default {
+		components: { NoData },
 		data() {
 			return {
 				dep: {},
@@ -76,7 +79,7 @@
 								console.log(this.rightNavData)
 							} else {
 								this.leftActive = 'nav' + this.navData[0].id
-								this.rightNavData = this.navData[0]
+								this.rightNavData = this.navData[0].symptoms
 							}
 						})
 				    }
