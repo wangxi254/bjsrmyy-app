257dc6ddd3fed4df1d8607345b171c741c805539
张庭勇 <wanmeizty@dingtalk.com>
2021-12-30 15:57:16 +0800

    删除无用数据

--- hospitalIntro/FloorList.vue
+++ hospitalIntro/FloorList.vue
@@ -16,13 +16,13 @@
         data() {
             return {
                 list: [
-					{img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
-					{img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
-					{img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
-					{img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
-					{img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
-					{img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
-					{img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
+					// {img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
+					// {img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
+					// {img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
+					// {img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
+					// {img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
+					// {img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
+					// {img:"http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg",title:"龙洞堡区住院楼"},
 				]
             }
         },
