89da04b106cc241a4fcc4e83e10eeeb1a0d7ebcc
seven <1310461255@qq.com>
2021-12-31 23:45:41 +0800

    科室问题

--- hospitalIntro/expertIntro.vue
+++ hospitalIntro/expertIntro.vue
@@ -78,11 +78,17 @@
 			getexpert(){
 				
 				let date = new Date().toISOString().slice(0, 10);
+				let time = new Date();
+				let num = 15;
+				var dates = new Date(time.setDate(time.getDate() + num)).getDate()  //这里先获取日期，在按需求设置日期，最后获取需要的
+				var year = time.getFullYear()  //获取年份
+				var month = time.getMonth() + 1   // 获取月份
+				const endtime = year + '-' + month + '-' + dates;
 				this.$request({
 					path:'/department/mobile/listNoPage',
 					query:{
 						beginDate:date,
-						endDate:date,
+						endDate:endtime,
 						regtype:1,
 						search:this.searchText,
 					}
