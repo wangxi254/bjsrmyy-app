f781bd5f1eb6cc9ed5c1f9bc752a597d3822812b
前端-谭浩 <542443538@qq.com>
2022-01-07 20:56:19 +0800

    ..

--- main.js
+++ main.js
@@ -29,41 +29,6 @@ Vue.prototype.$userInit = request.userInit;
 Vue.prototype.$getUserInfo = request.getUserInfo;
 Vue.prototype.$appUpdate = request.appUpdate;
 
-if (wx.canIUse('getUpdateManager')) {
-			const updateManager = wx.getUpdateManager()
-			updateManager.onCheckForUpdate(function (res) {
-				if (res.hasUpdate) {
-				updateManager.onUpdateReady(function () {
-					console.log('更新')
-					wx.showModal({
-					title: '更新提示',
-					content: '新版本已经准备好，是否重启应用？',
-					success: function (res) {
-						if (res.confirm) {
-						updateManager.applyUpdate()
-						}
-					}
-					})
-				})
-				updateManager.onUpdateFailed(function () {
-					console.log('已经是最新版本')
-					wx.showModal({
-					title: '已经有新版本了哟~',
-					content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
-					})
-				})
-				}
-				console.log(res)
-				console.log('获取更新信息完毕')
-			})
-			console.log('完毕')
-		} else {
-			wx.showModal({
-				title: '提示',
-				content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
-			})
-		}
-
 //测试
 Vue.prototype.$getUserId = ()=>{
     const userId = uni.getStorageSync("userId");
