d7b9f3e863cda85d4a4d241b6966de447d697c01
李元鹏 <1310461255@qq.com>
2022-01-20 16:03:19 +0800

    影像修改

--- test/index.vue
+++ test/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: seven
  * @Date: 2022-01-17 09:47:37
- * @LastEditTime: 2022-01-17 14:00:36
+ * @LastEditTime: 2022-01-18 09:32:26
  * @LastEditors: seven
  * @Description: 
  * @FilePath: \my-project\src\test\index.vue
@@ -22,6 +22,7 @@ export default {
         console.log('身份证base64加密结果',str)
         let timescamp = parseInt(new Date().getTime()/1000);
         console.log('当前时间10位时间戳',parseInt(new Date().getTime()/1000))
+        console.log(`${hospId}@${idCard}@www.newdcm.com`)
         let md5str = md5(`${hospId}@${idCard}@www.newdcm.com`);
         console.log('当前加密MD5',md5str)
         let uri = `http://61.189.240.138:9007/Visit/GetByIDCard?hospId=${hospId}&id=${str}&t=${timescamp}&co=${md5str}`;
