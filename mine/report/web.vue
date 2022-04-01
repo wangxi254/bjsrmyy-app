90c65225fceb656acb6b921eb7f1ad4d78967552
seven <1310461255@qq.com>
2022-04-02 00:04:20 +0800

    提交

--- mine/report/web.vue
+++ mine/report/web.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: seven
  * @Date: 2022-01-17 13:55:39
- * @LastEditTime: 2022-03-30 22:18:10
+ * @LastEditTime: 2022-04-01 23:22:39
  * @LastEditors: seven
  * @Description: 
  * @FilePath: \my-project\src\test\web.vue
@@ -26,11 +26,14 @@ export default {
         // return
         let hospId = "hl_bjsdyrmyy"
                 //this.credentialNo
-        let idCard = data;//"522421195307218048"
+        let idCard = '522421195307218048' //data;//"522421195307218048"
         let str = Base64.encode(idCard);
         let timescamp = parseInt(new Date().getTime()/1000);
         let md5str = md5(`${hospId}@${idCard}@www.newdcm.com@${timescamp}`);
-        let uri = `http://61.189.240.138:9007/Visit/GetByIDCard?hospId=${hospId}&id=${str}&t=${timescamp}&co=${md5str}`;
+        //let uri = `http://123.60.31.228/Visit/GetByIDCard?hospId=${hospId}&id=${str}&t=${timescamp}&co=${md5str}`;
+        //let uri = `http://61.189.240.138:9007/Visit/GetByIDCard?hospId=${hospId}&id=${str}&t=${timescamp}&co=${md5str}`;
+        let uri = `https://bjsyy.gzzxbd.com:9448/Visit/GetByIDCard?hospId=${hospId}&id=${str}&t=${timescamp}&co=${md5str}`;
+        console.log(uri)
         this.src = uri
     }
 }
