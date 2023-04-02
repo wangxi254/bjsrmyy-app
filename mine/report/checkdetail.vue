dbd10f41929a8021844f61bf76e4478f1f6b9c04
ccc_cjl <1349404751@qq.com>
2023-04-02 13:25:46 +0800

    检查报告添加图片和pdf附件展示，调整样式

--- mine/report/checkdetail.vue
+++ mine/report/checkdetail.vue
@@ -1,79 +1,272 @@
 <template>
 	<view>
-		<view class="paading15">
-			<view class="space-beteewn">
-				<view>就诊人：{{testReport.name}}</view>
-				<view>性别：{{testReport && testReport.sex == 'M'? '男':'女'}}</view>
-				<view>年龄：{{testReport.age}}</view>
+		<view class="root-container">
+			<view class="user-info-card">
+				<view class="name">{{testReport.name || 'xxx'}}</view>
+				<view class="sex">{{testReport && testReport.sex == 'M'? '男':'女'}}</view>
+				<view class="age">{{testReport.age || '--'}}岁</view>
 			</view>
-			<view>身份证号：</view>
-		</view>
-		
-		<view>报告单名称：{{testReport && testReport.reportName ? testReport.reportName:''}}</view>
-		<view>检验编号：{{testReport && testReport.reportCode ? testReport.reportCode:''}}</view>
-		
-		<view class="space-beteewn">
-			<view>科室名称：{{testReport && testReport.depName ? testReport.depName : ''}}</view>
-			<view>申请医生：{{testReport && testReport.sqDoc ? testReport.sqDoc : ''}}</view>
-		</view>
-		<view>检验时间：{{testReport && testReport.cyTime ? testReport.cyTime :''}}</view>
-		
-		<image v-if="testReport && testReport.picUrl" :src="testReport.picUrl"></image>
-		<view v-if="testReport && testReport.checkDetail">检查所见：{{testReport && testReport.checkDetail ? testReport.checkDetail : ''}}</view>
-		<view v-if="testReport && testReport.diagnosis">临床诊断：{{testReport && testReport.diagnosis ? testReport.diagnosis : ''}}</view>
-		<view v-if="testReport && testReport.qualitative">诊断结果：{{testReport && testReport.qualitative ? testReport.qualitative : ''}}</view>
-		<view class="space-beteewn">
-			<view>{{testReport && testReport.reportDoc ? testReport.reportDoc : ''}}</view>
-			<view>{{testReport && testReport.shDoc ? testReport.shDoc : ''}}</view>
+
+			<view class="label-container">
+				<view class="label-item">
+					<view class="label-txt">报告单名称：</view>
+					<view class="label-content">{{testReport && testReport.reportName ? testReport.reportName:''}}
+					</view>
+				</view>
+				<view class="label-item">
+					<view class="label-txt">检验编号：</view>
+					<view class="label-content">{{testReport && testReport.reportCode ? testReport.reportCode:''}}
+					</view>
+				</view>
+				<view class="label-item">
+					<view class="label-txt">科室名称：</view>
+					<view class="label-content">{{testReport && testReport.depName ? testReport.depName : ''}}</view>
+				</view>
+				<view class="label-item">
+					<view class="label-txt">申请医生：</view>
+					<view class="label-content">{{testReport && testReport.sqDoc ? testReport.sqDoc : ''}}</view>
+				</view>
+				<view class="label-item">
+					<view class="label-txt">检验时间：</view>
+					<view class="label-content">{{testReport && testReport.cyTime ? testReport.cyTime :''}}</view>
+				</view>
+
+				<view class="label-item" v-if="testReport && testReport.checkDetail">
+					<view class="label-txt">检查所见：</view>
+					<view class="label-content">{{testReport && testReport.checkDetail ? testReport.checkDetail : ''}}
+					</view>
+				</view>
+				<view class="label-item" v-if="testReport && testReport.diagnosis">
+					<view class="label-txt">临床诊断：</view>
+					<view class="label-content">{{testReport && testReport.checkDetail ? testReport.checkDetail : ''}}
+					</view>
+				</view>
+				<view class="label-item" v-if="testReport && testReport.qualitative">
+					<view class="label-txt">诊断结果：</view>
+					<view class="label-content">{{testReport && testReport.diagnosis ? testReport.diagnosis : ''}}
+					</view>
+				</view>
+				<view class="label-item" v-if="testReport && testReport.qualitative">
+					<view class="label-txt">报告医生：</view>
+					<view class="label-content">
+						{{testReport && testReport.reportDoc ? testReport.reportDoc : ''}}
+					</view>
+				</view>
+				<view class="label-item" v-if="testReport && testReport.qualitative">
+					<view class="label-txt">审核医生：</view>
+					<view class="label-content">{{testReport && testReport.shDoc ? testReport.shDoc : ''}}</view>
+				</view>
+			</view>
+
+			<view v-if="testReport && testReport.picUrl">
+				<!-- 图片 -->
+				<view v-if="testReport.type == 'image'" class="report-container">
+					<image :src="testReport.picUrl" mode='widthFix'></image>
+				</view>
+				<!-- PDF -->
+				<view v-if="testReport.type == 'pdf'" class="report-container-pdf">
+					<view class="open-btn" @click="openPdf">附件</view>
+					<!-- <web-view :src="testReport.picUrl"></web-view> -->
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import config from '../../config.js';
+
 	export default {
-	    data() {
-	        return {
-	            testReport:{}
-	        }
-	    },
-	    onLoad(options) {
-	        if(options.item){
+		data() {
+			return {
+				fileHost: '',
+				testReport: {}
+			}
+		},
+		onLoad(options) {
+			this.fileHost = config.baseUrl.replace('/api', '')
+			if (options.item) {
 				const item = JSON.parse(options.item);
 				this.getDetailInfoByPatient(item);
 			}
-			
-	    },
-	    methods: {
-	        // 容器滚动到底部
-	        getDetailInfoByPatient(item) {
-				
+		},
+		methods: {
+			openPdf() {
 				let that = this;
-	            let date = new Date().toISOString().slice(0, 10);
-	            this.$request({
-	            	path:'/checkReport/mobile/getDetailInfo',
-	            	query:{
-	            		// mrn:mrn,
-	            		reportSeq:item.reportSeq,
-	            		reportCode:item.reportCode,
+				// #ifdef MP-WEIXIN
+				uni.showLoading({
+					title: '加载中...'
+				})
+				if (that.cachePdfFilePath) {
+					wx.openDocument({
+						filePath: that.cachePdfFilePath,
+						fileType: 'pdf',
+						success: function(res) {
+							uni.hideLoading()
+						},
+						fail: function(err) {
+							uni.hideLoading()
+							console.log('fail:' + JSON.stringify(err));
+						}
+					});
+				} else {
+					wx.downloadFile({
+						url: this.testReport.picUrl,
+						success: function(res) {
+							uni.hideLoading()
+							that.cachePdfFilePath = res.tempFilePath;
+							uni.showLoading({
+								title: '正在打开',
+								mask: true
+							})
+							wx.openDocument({
+								filePath: that.cachePdfFilePath,
+								fileType: 'pdf',
+								success: function(res) {
+									console.log(res)
+									uni.hideLoading()
+								},
+								fail: function(err) {
+									uni.hideLoading()
+									console.log('fail:' + JSON.stringify(err));
+								}
+							});
+						},
+						fail: function(err) {
+							uni.hideLoading()
+							console.log('fail:' + JSON.stringify(err));
+						}
+					});
+				}
+				// #endif
+
+				// // #ifndef MP-WEIXIN
+				// uni.openDocument({
+				// 	filePath: this.testReport.picUrl,
+				// 	filetype: "pdf",
+				// 	showMenu: true,
+				// 	success: function(file) {
+				// 		console.log("file-success", file)
+				// 	},
+				// 	fail: function(file) {
+				// 		console.log("file-fail", file)
+				// 	}
+				// })
+				// // #endif
+			},
+
+			getDetailInfoByPatient(item) {
+				let that = this;
+				let date = new Date().toISOString().slice(0, 10);
+				this.$request({
+					path: '/checkReport/mobile/getDetailInfo',
+					query: {
+						reportSeq: item.reportSeq,
+						reportCode: item.reportCode,
 						// userId:uni.getStorageSync("userId"),
-						mrn:item.mrn,
-	            	}
-	            }).then(res=>{
-					console.log("getDetailInfo:res====>",JSON.stringify(res));
-	            	if(res.data.code == 200){
-						that.testReport = res.data.data;
-					}else{
+						mrn: item.mrn,
+					}
+				}).then(res => {
+					if (res.data.code == 200) {
+						let resultData = res.data.data
+						resultData.picUrl = this.fileHost + resultData.picUrl
+						that.testReport = resultData;
+					} else {
 						uni.showToast({
-							icon:'none',
-							title:res.data.msg
+							icon: 'none',
+							title: res.data.msg
 						})
 					}
-	            })
-	        },
-	    }
+				})
+			},
+		}
 	}
-	
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.root-container {
+		padding: 10px 5px 30px 5px;
+	}
+
+	.label-container {
+		border: 1px #e7e7e7 solid;
+		border-radius: 2px;
+		margin-top: 10px;
+
+		.label-item {
+			display: flex;
+			font-size: 13px;
+			border-bottom: 1px #e7e7e7 solid;
+
+			.label-txt {
+				min-width: 85px;
+				height: 100%;
+				color: #666666;
+				padding: 12px 10px;
+			}
+
+			.label-content {
+				display: flex;
+				align-items: center;
+				color: #000000;
+				min-height: 20px;
+				padding: 5px 15px;
+				word-wrap: break-word;
+				border-left: 1px #e7e7e7 solid;
+			}
+		}
+
+		.label-item:last-child {
+			border-bottom: none;
+		}
+	}
+
+	.user-info-card {
+		display: flex;
+		align-items: center;
+		color: #000000;
+		font-size: 15px;
+		border-radius: 5px;
+		padding: 20px;
+		margin: 5px;
+		margin-bottom: 20px;
+		box-shadow: 0px 2px 10px #e7e7e7;
+
+		.name {
+			font-size: 17px;
+			font-weight: bold;
+		}
+
+		.age {
+			margin-left: 10px;
+		}
+
+		.sex {
+			margin-left: 30px;
+		}
+	}
+
+	.report-container {
+		width: 100vw;
+		min-height: 600px;
+		margin-bottom: 20px;
+
+		image {
+			width: 100vw;
+		}
+	}
+
+	.report-container-pdf {
+		width: 100vw;
+
+		.open-btn {
+			width: 100%;
+			height: 50px;
+			line-height: 50px;
+			text-align: center;
+			font-size: 14px;
+			color: blueviolet;
+			font-weight: bold;
+		}
+	}
 </style>
--- mine/report/checkreport.vue
+++ mine/report/checkreport.vue
@@ -3,12 +3,14 @@
 		<view class="head-view">
 			<picker :value="credentialTypeIndex" :range="patientList" @change="credentialTypeChange" range-key="name">
 				<view class="picker-view paadinglr15">
-				  <view class="font-size-16-w600 text-color-333333">
+					<view class="font-size-16-w600 text-color-333333">
 						{{patientList[credentialTypeIndex].name||'选择联系人类型'}}
-				  </view>
-				  <view class="flex1 marginl10 text-color-333333 font-size-14-w400">{{ utils.hideIdentityCard(patientList[credentialTypeIndex].credentialNo)}}</view>
-				  <view class="font-size-14-w400 text-color-8f8f8f">切换</view>
-				  <image class="right marginl10" src="../../static/common/exchange.png"></image>
+					</view>
+					<view class="flex1 marginl10 text-color-333333 font-size-14-w400">
+						{{ utils.hideIdentityCard(patientList[credentialTypeIndex].credentialNo)}}
+					</view>
+					<view class="font-size-14-w400 text-color-8f8f8f">切换</view>
+					<image class="right marginl10" src="../../static/common/exchange.png"></image>
 				</view>
 			</picker>
 		</view>
@@ -23,7 +25,7 @@
 				</view>
 			</picker>
 			<picker mode="date" :value="endDate" @change="bindEDateChange">
-				
+
 				<view class="picker-view height50">
 					<view class="font-size-14-w400 text-color-8f8f8f">结束：</view>
 					<view class="font-size-14-w400 text-color-333333">
@@ -33,7 +35,7 @@
 				</view>
 			</picker>
 		</view>
-		
+
 		<view v-for="item in reportlist">
 			<view class="cell" @click="getDetailInfo(item)">
 				<view class="space-between border-bottom paddingb15">
@@ -58,13 +60,13 @@
 		data() {
 			return {
 				utils,
-				userInfo:{
-					name:"张某某",
-					idcard:'522122xxxxxxxxx0011'
+				userInfo: {
+					name: "张某某",
+					idcard: '522122xxxxxxxxx0011'
 				},
-				startDate:new Date().toISOString().slice(0, 10),
-				endDate:new Date().toISOString().slice(0, 10),
-				reportlist:[
+				startDate: null,
+				endDate: new Date().toISOString().slice(0, 10),
+				reportlist: [
 					// {
 					// 	depName:'外科检查',
 					// 	reportTime:'2021-12-20',
@@ -78,17 +80,21 @@
 					// 	reportCode:'00003283434834',
 					// }
 				],
-				patientList:[],
-				credentialTypeIndex:0,
-				credentialType:'',
-				credentialText:'',
-				credentialNo:'',
-				mrn:'',
+				patientList: [],
+				credentialTypeIndex: 0,
+				credentialType: '',
+				credentialText: '',
+				credentialNo: '',
+				mrn: '',
 			}
 		},
 		onLoad(options) {
-			console.log("options.item",options.item);
-			if(options.item){
+			const dates = new Date()
+			dates.setMonth(dates.getMonth() - 3)
+
+			this.startDate = dates.toISOString().slice(0, 10)
+
+			if (options.item) {
 				let item = JSON.parse(options.item);
 				// this.mrn = item.mrn;
 			}
@@ -97,228 +103,228 @@
 			// this.getCheckreportbyUserId();
 		},
 		methods: {
-			async requestList(){
-				const [err,res] = await this.$arequest({
-					path:"/patient/mobile/getPatientByUserId",
-					query:{
-						userId:uni.getStorageSync("userId"),
+			async requestList() {
+				const [err, res] = await this.$arequest({
+					path: "/patient/mobile/getPatientByUserId",
+					query: {
+						userId: uni.getStorageSync("userId"),
 					}
 				});
 				let defaultPatientItem;
-				if(res.data.code == 200){
+				if (res.data.code == 200) {
 					const list = res.data.data;
-					if(list && list.length > 0){
+					if (list && list.length > 0) {
 						this.patientList = list;
-						console.log("list===>",JSON.stringify(list));
-						for(let i = 0; i < list.length; i ++){
+						console.log("list===>", JSON.stringify(list));
+						for (let i = 0; i < list.length; i++) {
 							const item = list[i];
-							
-							if(item.defaultPatient == 1){
+
+							if (item.defaultPatient == 1) {
 								defaultPatientItem = item;
 								this.credentialTypeIndex = i;
 								break;
 							}
 						}
-						
-						if(defaultPatientItem==null){
-							 defaultPatientItem = list[0];
+
+						if (defaultPatientItem == null) {
+							defaultPatientItem = list[0];
 						}
 					}
 				}
-				
+
 				let req = {
-					condition:defaultPatientItem.credentialNo,
-					conditionType:defaultPatientItem.credentialType,
+					condition: defaultPatientItem.credentialNo,
+					conditionType: defaultPatientItem.credentialType,
 				}
 				this.credentialNo = defaultPatientItem.credentialNo;
 				this.credentialType = defaultPatientItem.credentialType;
 				this.getreport();
 			},
 			bindSDateChange(e) {
-			  let date = e.detail.value;
-			  this.startDate = date;
-			  this.reportlist = [];
-			  if(this.judgeCondition()){
-			  	this.search();
-			  }
+				let date = e.detail.value;
+				this.startDate = date;
+				this.reportlist = [];
+				if (this.judgeCondition()) {
+					this.search();
+				}
 			},
 			bindEDateChange(e) {
-			  let date = e.detail.value;
-			  this.endDate = date;
-			  this.reportlist = [];
-			  if(this.judgeCondition()){
-			  	this.search();
-			  }
+				let date = e.detail.value;
+				this.endDate = date;
+				this.reportlist = [];
+				if (this.judgeCondition()) {
+					this.search();
+				}
 			},
-			getCheckreport(mrn){
+			getCheckreport(mrn) {
 				let that = this;
 				uni.showLoading();
 				this.$request({
-					path:'/checkReport/mobile/getInfo',
-					query:{
-						beginDate:this.startDate,
-						endDate:this.endDate,
-						mrn:mrn,
+					path: '/checkReport/mobile/getInfo',
+					query: {
+						beginDate: this.startDate,
+						endDate: this.endDate,
+						mrn: mrn,
 					}
-				}).then(res=>{
+				}).then(res => {
 					uni.hideLoading()
-					if(res.data.code == 200){
+					if (res.data.code == 200) {
 						that.reportlist = res.data.data;
 					}
 				})
 			},
-			getCheckreportbyUserId(){
-				
-				if(this.startDate.length == 0){
+			getCheckreportbyUserId() {
+
+				if (this.startDate.length == 0) {
 					return uni.showToast({
-						icon:"none",
-						title:"请选择开始时间"
+						icon: "none",
+						title: "请选择开始时间"
 					})
 				}
-				
-				if(this.endDate.length == 0){
+
+				if (this.endDate.length == 0) {
 					return uni.showToast({
-						icon:"none",
-						title:"请选择结束时间"
+						icon: "none",
+						title: "请选择结束时间"
 					})
 				}
-				
-				let that = this;  
+
+				let that = this;
 				this.$request({
-					path:'/checkReport/mobile/getInfoByPatient',
-					query:{
-						beginDate:this.startDate,
-						endDate:this.endDate,
-						userId:uni.getStorageSync("userId"),
+					path: '/checkReport/mobile/getInfoByPatient',
+					query: {
+						beginDate: this.startDate,
+						endDate: this.endDate,
+						userId: uni.getStorageSync("userId"),
 					}
-				}).then(res=>{
-					if(res.data.code == 200){
+				}).then(res => {
+					if (res.data.code == 200) {
 						that.reportlist = res.data.data;
 					}
 				})
 			},
-			judgeCondition(){
-				if(this.startDate.length == 0){
+			judgeCondition() {
+				if (this.startDate.length == 0) {
 					return false;
 				}
-				
-				if(this.endDate.length == 0){
+
+				if (this.endDate.length == 0) {
 					return false;
 				}
-				
-				if(this.credentialNo.length == 0){
+
+				if (this.credentialNo.length == 0) {
 					return false;
-				}	
+				}
 				this.reportlist = [];
 				return true;
 			},
-			search(){
+			search() {
 				// if(this.mrn.length > 0){
 				// 	this.getCheckreport(this.mrn);
 				// }else{
 				// 	this.requestList();
 				// }
 				// this.getCheckreportbyUserId();
-				if(this.startDate.length == 0){
+				if (this.startDate.length == 0) {
 					return uni.showToast({
-						icon:"none",
-						title:"请选择开始时间"
+						icon: "none",
+						title: "请选择开始时间"
 					})
 				}
-				
-				if(this.endDate.length == 0){
+
+				if (this.endDate.length == 0) {
 					return uni.showToast({
-						icon:"none",
-						title:"请选择结束时间"
+						icon: "none",
+						title: "请选择结束时间"
 					})
 				}
-				
-				if(this.credentialNo.length == 0){
+
+				if (this.credentialNo.length == 0) {
 					return uni.showToast({
-						icon:"none",
-						title:"选择就诊人"
+						icon: "none",
+						title: "选择就诊人"
 					})
 				}
-			
+
 				this.getreport();
 			},
-			async getreport(){
+			async getreport() {
 				let that = this;
 				let req = {
-					condition:this.credentialNo,
-					conditionType:this.credentialType,
+					condition: this.credentialNo,
+					conditionType: this.credentialType,
 				}
-				const [perr,pres] = await this.$arequest({
-					path:"/tpatientCard/mobile/getPatientCardByPatientInfo",
-					query:req,
+				const [perr, pres] = await this.$arequest({
+					path: "/tpatientCard/mobile/getPatientCardByPatientInfo",
+					query: req,
 				})
-				console.log("res",JSON.stringify(pres));
-				if(pres.data.code == 200){
+				console.log("res", JSON.stringify(pres));
+				if (pres.data.code == 200) {
 					const data = pres.data.data;
 					const mrn = data.mrn
 					this.mrn = mrn;
-					console.log("mrn===>",mrn);
+					console.log("mrn===>", mrn);
 					that.getCheckreport(mrn);
 				}
 			},
-			getDetailInfo(item){
-				
-			 	const credentialTyps = [
+			getDetailInfo(item) {
+
+				const credentialTyps = [
 					// {
 					// 	value:0,
 					// 	name:'未知',
 					// },
 					{
-						value:1,
-						name:'身份证'
+						value: 1,
+						name: '身份证'
 					},
 					{
-						value:2,
-						name:'军官证',
+						value: 2,
+						name: '军官证',
 					},
 					{
-						value:3,
-						name:'户口本'
+						value: 3,
+						name: '户口本'
 					},
 					{
-						value:4,
-						name:'护照',
+						value: 4,
+						name: '护照',
 					},
 					{
-						value:5,
-						name:'外国人永久居住证'
+						value: 5,
+						name: '外国人永久居住证'
 					},
 					{
-						value:6,
-						name:'就诊卡号',
+						value: 6,
+						name: '就诊卡号',
 					},
 					{
-						value:7,
-						name:'住院号'
+						value: 7,
+						name: '住院号'
 					},
 					{
-						value:8,
-						name:'病历号'
+						value: 8,
+						name: '病历号'
 					}
 				];
 				const credentialType = this.credentialType;
-				let credentialText  = '';
-				credentialTyps.forEach(item=>{
-					if(item.value == credentialType){
+				let credentialText = '';
+				credentialTyps.forEach(item => {
+					if (item.value == credentialType) {
 						credentialText = item.name;
 					}
 				})
 				let req = {
-					beginDate:this.startDate,
-					endDate:this.endDate,
-					mrn:this.mrn,
-					credentialType:this.credentialType,
-					credentialNo:this.credentialNo,
-					credentialText:credentialText,
+					beginDate: this.startDate,
+					endDate: this.endDate,
+					mrn: this.mrn,
+					credentialType: this.credentialType,
+					credentialNo: this.credentialNo,
+					credentialText: credentialText,
 					...item
 				}
 				uni.navigateTo({
-					url:`checkdetail?item=${JSON.stringify(req)}`
+					url: `checkdetail?item=${JSON.stringify(req)}`
 				})
 				// this.$request({
 				// 	path:'/checkReport/mobile/getDetailInfoByPatient',
@@ -327,63 +333,62 @@
 				// 		beginDate:this.startDate,
 				// 		endDate:this.endDate,
 				// 	}
-					
+
 				// }).then(res=>{
-					
+
 				// })
 			},
-			credentialTypeChange(e){
-				console.log("e===>",JSON.stringify(e));
+			credentialTypeChange(e) {
+				console.log("e===>", JSON.stringify(e));
 				const index = e.detail.value;
-				this.credentialTypeIndex =  index;
+				this.credentialTypeIndex = index;
 				this.credentialType = this.patientList[index].credentialType;
 				this.credentialNo = this.patientList[index].credentialNo;
-				if(this.judgeCondition()){
+				if (this.judgeCondition()) {
 					this.search();
 				}
 			}
-			
+
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	.right{
+	.right {
 		height: 10px;
 		width: 10px;
 	}
-	
-	.head-view{
+
+	.head-view {
 		height: 52px;
 		background: #FFFFFF;
 	}
-	
-	.picker-view{
+
+	.picker-view {
 		align-items: center;
 		display: flex;
 		justify-content: center;
 		height: 52px;
-		
+
 	}
-	
-	.height50{
+
+	.height50 {
 		height: 50px;
 	}
-	
-	.height40{
+
+	.height40 {
 		height: 50px;
 	}
-	
-	
-	
-	.margin{
+
+
+
+	.margin {
 		margin: 10px 15px;
 	}
-	
-	.cell{
+
+	.cell {
 		margin: 15px 15px 0px 15px;
 		padding: 15px;
 		background: #FFFFFF;
 	}
-	
 </style>
